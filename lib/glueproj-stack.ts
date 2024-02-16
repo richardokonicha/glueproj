891f64d549b2819061922bf0275c92c8b7ddf505
Rich <richardokonicha@gmail.com>
2024-02-16 23:24:33 +0100

    complete

--- lib/glueproj-stack.ts
+++ lib/glueproj-stack.ts
@@ -1,16 +1,67 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as glue from 'aws-cdk-lib/aws-glue';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class GlueprojStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const dbname = "mydatabase"
+    // Create Glue database
+    const database = new glue.CfnDatabase(this, 'MyDatabase', {
+      catalogId: cdk.Stack.of(this).account,
+      databaseInput: {
+        name: dbname
+      }
+    })
 
-    // The code that defines your stack goes here
+    // Create Role
+    const glueRole = new iam.Role(this, 'GlueCrawlerRole', {
+      assumedBy: new iam.ServicePrincipal('glue.amazonaws.com')
+    })
+    glueRole.addToPolicy(new iam.PolicyStatement({
+      actions: ['s3:GetObject',
+        "s3:ListBucket",
+        "s3:GetObject",
+        "s3:GetBucketLocation",
+        "s3:ListMultipartUploadParts",
+        "logs:PutLogEvents",
+        "glue:*"
+      ],
+      resources: ['*']
+    }))
+    // Create Glue crawler 
+    const crawler = new glue.CfnCrawler(this, 'MyCrawler', {
+      databaseName: dbname,
+      role: glueRole.roleArn,
+      targets: {
+        s3Targets: [
+          {
+            path: 's3://listbucketlistbucket12345/data/'
+          }
+        ]
+      },
+      // Update new and existing partitions with metadata from the table
+      configuration: JSON.stringify({
+        Version: 1.0,
+        Grouping: {
+          TableGroupingPolicy: 'CombineCompatibleSchemas'
+        },
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'GlueprojQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+        CrawlerOutput: {
+          Partitions: {
+            AddOrUpdateBehavior: 'InheritFromTable'
+          }
+        }
+      }),
+      schemaChangePolicy: {
+        updateBehavior: 'LOG',
+      },
+      // schedule: {
+      //   scheduleExpression: 'cron(0 0 * * ? *)'
+      // }
+    })
+
+    console.log('Crawler Name: ' + crawler.logicalId)
   }
 }
